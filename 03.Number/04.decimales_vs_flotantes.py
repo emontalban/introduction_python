ef066919253d8f2d4457d142432b6fcf060ba274
emontalban <shamanessemmp@gmail.com>
2026-03-05 10:38:56 +0100

    añadido varios archivos

--- 03.Number/04.decimales_vs_flotantes.py
+++ 03.Number/04.decimales_vs_flotantes.py
@@ -5,18 +5,26 @@
 
 from decimal import Decimal
 
-product_cost = 88.40
+product_cost_float = 88.40
 commission_rate = 0.08
 qty = 450
 
-product_cost += (commission_rate * product_cost)
-print(product_cost * qty) # 42962.4
+product_cost_float += (commission_rate * product_cost_float)
+print(product_cost_float * qty) # 42962.4
 
-product_cost = Decimal(88.40)
+product_cost_decimal = Decimal(88.40)
 commission_rate = Decimal(0.08)
 qty = 450
 
-product_cost += (commission_rate * product_cost)
-print(product_cost * qty) # 42962.40000000000282883716451
+product_cost_decimal += (commission_rate * product_cost_decimal)
+print(product_cost_decimal * qty) # 42962.40000000000282883716451
+
+
+#Podemos convertir las varibles a otros tipos de datos pon 
+
+print(int(product_cost_float))
+print(float(qty))
+print(Decimal(product_cost_float))
+print(complex(commission_rate))
 
 
